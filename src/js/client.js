5c0c251e3b3d4369b59dffab65be99e1568855ad
Ignacio Gomez <ignaciob.gomez@gmail.com>
2018-12-02 16:38:51 -0300

    smooth mouse

--- src/js/client.js
+++ src/js/client.js
@@ -1,6 +1,46 @@
 //comenzamos
 $(document).ready(function() {
 
+
+var $box = $('.box'),
+  inter = 30,
+  speed = 0;
+
+function moveBox(e) {
+  //TweenMax.killTweensOf();
+  $box.each(function(index, val) {
+   TweenLite.to($(this), 0.05, { css: { left: e.pageX, top: e.pageY},delay:0+(index/750)});
+  });
+}
+
+$(window).on('mousemove', moveBox);
+
+$box.each(function(index, val) {
+    index = index + 1;
+    TweenMax.set(
+      $(this),{
+        autoAlpha: 1 - (0.0333 * index),
+        delay:0
+      });
+  });
+  TweenMax.set(
+    $('.text:nth-child(30)'), {
+      autoAlpha: 1.5,
+      delay: 0
+    }
+  );
+
+//V2.0
+$('.pointer').mouseover(function(){
+  //console.log('cursor');
+  $box.addClass('arrow');
+});
+$('.circle').mouseover(function(){
+  //console.log('circle');
+  $box.removeClass('arrow');
+});
+
+
 	 //redefinimos array para añadir un random, lo vamos a usar...
 	 Array.prototype.rand = function () {
 	 	return this[Math.floor((Math.random()*this.length))];
